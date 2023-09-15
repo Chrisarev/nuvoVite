71a96e352e6e9a6c76b39ccc48999327d1c7f304
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-09-14 17:20:43 -0700

    Removed unnecessary code from app.js

--- app.js
+++ app.js
@@ -4,19 +4,6 @@ dotenv.config();
 const express = require('express');
 const path = require('path');
 
-/***************** DATABASE CONFIG *****************/
-const dbUrl = process.env.DB_URL;
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});///creates db yelp-camp(or connects to it if already made)
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database Connected")
-});///checks to see if connected and handles db connection error
-
 /***************** MIDDLEWARE CONFIG *****************/
 const app = express();
 app.use(express.json({ limit: "50mb" }))
